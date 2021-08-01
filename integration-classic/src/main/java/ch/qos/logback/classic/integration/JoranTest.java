4b5c471f238a58fa462dda13846edd0eca9bb2ea
Ceki Gulcu <ceki@qos.ch>
2021-08-01 23:35:57 +0200

    ow

--- integration-classic/src/main/java/ch/qos/logback/classic/integration/JoranTest.java
+++ integration-classic/src/main/java/ch/qos/logback/classic/integration/JoranTest.java
@@ -1,5 +1,7 @@
 package ch.qos.logback.classic.integration;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
@@ -8,7 +10,11 @@ import org.junit.Test;
 import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.joran.JoranConfigurator;
+import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.joran.spi.JoranException;
+import ch.qos.logback.core.read.ListAppender;
+import ch.qos.logback.core.status.Status;
+import ch.qos.logback.core.status.StatusUtil;
 
 public class JoranTest {
 
@@ -41,5 +47,25 @@ public class JoranTest {
 	@Test
 	public void simpleList() throws JoranException {
       configure(IntegrationConstants.JORAN_INPUT_PREFIX + "simpleList.xml");
+      Logger logger = loggerContext.getLogger(this.getClass().getName());
+		Logger root = loggerContext.getLogger(Logger.ROOT_LOGGER_NAME);
+		ListAppender<ILoggingEvent> listAppender = (ListAppender<ILoggingEvent>) root.getAppender("LIST");
+		assertNotNull(listAppender);
+		assertEquals(0, listAppender.list.size());
+		String msg = "hello world";
+		logger.debug(msg);
+		assertEquals(1, listAppender.list.size());
+		ILoggingEvent le = (ILoggingEvent) listAppender.list.get(0);
+		assertEquals(msg, le.getMessage());
+	}
+	
+	@Test
+	public void statusListener() throws JoranException {
+		configure(IntegrationConstants.JORAN_INPUT_PREFIX + "statusListener.xml");
+		
+		StatusUtil statusUtil = new StatusUtil(loggerContext);
+		assertTrue(statusUtil.isErrorFree(0));
+		assertTrue(statusUtil.containsMatch(Status.WARN,
+				"Please use \"level\" attribute within <logger> or <root> elements instead."));
 	}
 }
